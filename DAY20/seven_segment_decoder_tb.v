5d3f0a5a5531b8e4ff9433d11f75599f0da84565
DARSHAN-cod <sundardarshan14@gmail.com>
2025-11-20 22:51:46 +0530

    Create seven_segment_decoder_tb.v

---
+++ DAY20/seven_segment_decoder_tb.v
@@ -0,0 +1,24 @@
+// Code your testbench here
+// or browse Examples
+module seven_segment_decoder_tb;
+  reg  [3:0] bcd_in;
+  wire [6:0] seg_out;
+
+seven_segment_decoder dut (.bcd_in(bcd_in),.seg_out(seg_out));
+initial begin
+  $display("Time  BCD  Segment");
+  $monitor("%0t  %b  %b", $time, bcd_in, seg_out);
+    bcd_in = 4'd0; #10;
+    bcd_in = 4'd1; #10;
+    bcd_in = 4'd2; #10;
+    bcd_in = 4'd3; #10;
+    bcd_in = 4'd4; #10;
+    bcd_in = 4'd5; #10;
+    bcd_in = 4'd6; #10;
+    bcd_in = 4'd7; #10;
+    bcd_in = 4'd8; #10;
+    bcd_in = 4'd9; #10;
+    bcd_in = 4'd12; #10;
+    $finish;
+end
+endmodule
