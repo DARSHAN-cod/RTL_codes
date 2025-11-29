db5d0f198537d8ad4256fb014190d68e1779d077
DARSHAN-cod <sundardarshan14@gmail.com>
2025-11-29 22:21:27 +0530

    Add ring_counter module with reset functionality

---
+++ DAY_28/ring_counter.v
@@ -0,0 +1,14 @@
+// Code your design here
+module ring_counter(
+  input clk,input reset,output reg [3:0]q);
+  always@(posedge clk or posedge reset)
+    begin
+      if(reset)
+        q<=4'b1000;
+      else
+        q<={q[2:0],q[3]};
+    end
+endmodule
+
+
+
